82aa82daaaef01484d27618c233af391dcc3e81a
Michael Domingues <michael.domingues@matera.com>
2015-06-15 01:56:26 -0300

    mais um monte de correção

--- mybeer-dev_site/mybeerdev/app/avaliacoes/avaliacoes.js
+++ mybeer-dev_site/mybeerdev/app/avaliacoes/avaliacoes.js
@@ -5,17 +5,30 @@ angular.module('myApp.avaliacoes', ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/avaliacoes', {
         templateUrl: 'avaliacoes/avaliacoes.html',
-        controller: 'PromocoesController'
+        controller: 'AvaliacoesCtrl'
     });
     }])
 
-    .controller('PromocoesController', ['$scope','$http','promocoesService','toaster', function ($scope,$http,promocoesService, toaster) {
+    .controller('AvaliacoesCtrl', ['$scope','$http','avaliacoesService','toaster','loginService','nomeBanco', function ($scope,$http,avaliacoesService, toaster,loginService, nomeBanco) {
 
-        
-    
+        function getAvaliacoes() {
+            avaliacoesService.getAvaliacoes(loginService.getId())
+                .success(function (data) {
+                $scope.avaliacoes = data;
+                console.log(data);
+            })
+                .error(function (error) {
+                console.log(error.message);
+            });
+        }
 
-    }]).service("promocoesService", function ($http, $q) {
+        getAvaliacoes();
+
+
+    }]).service("avaliacoesService", function ($http, $q,nomeBanco) {
   
-
+    this.getAvaliacoes = function (id) {
+        return $http.get(nomeBanco.getLink() + 'clientes/avaliacoes/' + id);
+    }
 
 });
--- mybeer-dev_site/mybeerdev/app/eventos/eventos.js
+++ mybeer-dev_site/mybeerdev/app/eventos/eventos.js
@@ -9,16 +9,16 @@ angular.module('myApp.eventos', ['ngRoute'])
     });
     }])
 
-    .controller('eventoController', ['$scope','$http','eventoService','toaster','nomeBanco', function ($scope,$http,eventoService, toaster,nomeBanco) {
+.controller('eventoController', ['$scope', '$http', 'eventoService', 'toaster', 'nomeBanco', 'loginService', function ($scope, $http, eventoService, toaster, nomeBanco, loginService) {
 
 
     $scope.eventos = [];
     $scope.evento = {};
     $scope.alerts = [];
-        
-        //hora
-        $scope.mytime = new Date();
-        $scope.ismeridian = false;
+
+    //hora
+    $scope.mytime = new Date();
+    $scope.ismeridian = false;
 
     $scope.conversorDate = function (data) {
 
@@ -29,7 +29,7 @@ angular.module('myApp.eventos', ['ngRoute'])
             var dYear = d.getFullYear();
             var dHour = $scope.mytime.getHours();
             var dMinute = $scope.mytime.getMinutes();
-            var date = dDay + "/" + dMon + "/" + dYear + " " + dHour +":"+ dMinute;
+            var date = dDay + "/" + dMon + "/" + dYear + " " + dHour + ":" + dMinute;
 
             return date;
         }
@@ -39,65 +39,106 @@ angular.module('myApp.eventos', ['ngRoute'])
     $scope.limparForm = function () {
         $scope.evento = {};
     }
-    
+
     $scope.sendEvento = function (evento) {
 
         var res = $http.put(nomeBanco.getLink() + 'eventos', evento);
         res.success(function (data, status, headers, config) {
 
             $scope.limparForm();
-            
-           toaster.pop('success', "Sucesso", "Evento adicionado com sucesso");
-        
+
+            toaster.pop('success', "Sucesso", "Evento adicionado com sucesso");
+
             var message = data;
         });
         res.error(function (data, status, headers, config) {
 
-           
-            toaster.pop('error', "Erro Interno", JSON.stringify({data:data}));
+
+            toaster.pop('error', "Erro Interno", JSON.stringify({
+                data: data
+            }));
 
 
         });
-        
+
         $scope.getEventos();
     }
 
-    $scope.criarEventos = function(){
-        
+    $scope.criarEventos = function () {
+
         $scope.evento.data = $scope.conversorDate($scope.evento.data);
         var evento = angular.copy($scope.evento);
         //PEGAR CODIGO DO BAR
         evento.codbar = 1;
         $scope.sendEvento(evento);
     }
-    
-    var mesesString = ["JAN","FEV","MAR","ABR","MAI","JUN","JUL","AGO","SET","OUT","NOV","DEZ"];
-    
-        $scope.getEventos = function(){
-            var promise = eventoService.getEventos();
-            promise.then(function (data) {
-                $scope.eventos = data.data.evento;  
-                angular.forEach($scope.eventos, function(value, key) {
-                    value.dia = value.data.substring(0,2);
-                    value.mes = value.data.substring(3,5);
-                    value.mesString = mesesString[Number(value.mes)-1];
-                    value.ano = value.data.substring(6,10);
-                });
-            })
-        }
- 
 
-        $scope.getEventos();
+    var mesesString = ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"];
 
-    }]).service("eventoService", function ($http, $q,nomeBanco) {
-    var deferred = $q.defer();
-    $http.get(nomeBanco.getLink() + 'eventos').then(function (data) {
-        deferred.resolve(data);
-    });
-
-    this.getEventos = function () {
-        return deferred.promise;
+   
+    function getUser() {
+        eventoService.getDadosUser(loginService.getId())
+            .success(function (data) {
+            $scope.usuario = data;
+            console.log(data);
+        })
+            .error(function (error) {
+            console.log(error.message);
+        });
     }
 
+    getUser();
+
+    function getEventosCliente() {
+        eventoService.getEventos(loginService.getId())
+            .success(function (data) {
+                $scope.eventos = data;
+                angular.forEach($scope.eventos, function (value, key) {
+                    value.dia = value.data.substring(0, 2);
+                    value.mes = value.data.substring(3, 5);
+                    value.mesString = mesesString[Number(value.mes) - 1];
+                    value.ano = value.data.substring(6, 10);
+                });
+            })
+            .error(function (error) {
+                console.log(error.message);
+            });
+    }
+    
+
+    function getEventosFuncionario() {
+        eventoService.getEventos(loginService.getId())
+            .success(function (data) {
+            $scope.eventos = data;
+            angular.forEach($scope.eventos, function (value, key) {
+                value.dia = value.data.substring(0, 2);
+                value.mes = value.data.substring(3, 5);
+                value.mesString = mesesString[Number(value.mes) - 1];
+                value.ano = value.data.substring(6, 10);
+            });
+        })
+            .error(function (error) {
+            console.log(error.message);
+        });
+    }
+
+    if($scope.usuario.codbar != null){
+        getEventosCliente();
+    }else{
+        getEventosFuncionario();
+    }
+    
+
+    }]).service("eventoService", function ($http, $q, nomeBanco) {
+
+
+    this.getEventos = function (id) {
+        return $http.get(nomeBanco.getLink() + 'clientes/eventos/favoritos/' + id);
+    }
+    
+    this.getDadosUser = function (id) {
+        return $http.get(nomeBanco.getLink() + 'pessoas/' + id);
+
+    }
 
 });
